feff66c671a78afd785f12785c27b17437cc14fb
Alfaro-dev <carlos.alfaro2605@gmail.com>
2024-12-16 17:48:56 -0600

    add cors validation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+const express = require('express');
 require('dotenv').config();
-const app = require('./app');
-const cors = require('cors');
 
-const corsOptions = {
-  origin: '*',
-};
+const app = express();
 
-app.use(cors(corsOptions));
+// Middleware global para agregar encabezados CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Permitir cualquier origen
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Server is running!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
